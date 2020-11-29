947d60c30e89ad87b66c34233c2af4a92290ec00
Vuong Huynh <hoangvuong19991964@gmail.com>
2020-11-29 09:32:24 +0700

    Fix OOP design in class LowLightEnhance

--- OOP-ImageEnhancer/frame_enhancer.py
+++ OOP-ImageEnhancer/frame_enhancer.py
@@ -6,9 +6,14 @@ import numpy as np
 from PIL import Image
 
 class LowLightEnhance():
-    def __init__(self, gpu_id = -1, model_path = ''):
+    def __init__(self, gpu_id = -1, model_path):
         self.gpu_id  = gpu_id
         self.model_path = model_path
+        
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(self.gpu_id)
+        
+        self.DCE_net = model.enhance_net_nopool().cuda()
+        self.DCE_net.load_state_dict(torch.load(self.model_path))
         
     def torch_tensor_to_image(self, tensor):
         img = tensor.cpu().data.numpy()[0]
@@ -16,20 +21,13 @@ class LowLightEnhance():
         return img
         
     def enhance(self, frame):
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(self.gpu_id)
         data_lowlight = Image.fromarray(frame)
         data_lowlight = (np.asarray(data_lowlight) / 255.0)
         data_lowlight = torch.from_numpy(data_lowlight).float()
         data_lowlight = data_lowlight.permute(2, 0, 1)    
         data_lowlight = data_lowlight.cuda().unsqueeze(0)
         
-        if self.model_path == '':
-            print('error when load model!')
-            return 0
-        DCE_net = model.enhance_net_nopool().cuda()
-        DCE_net.load_state_dict(torch.load(self.model_path))
-        
-        _,enhanced_image,_ = DCE_net(data_lowlight)    
+        _,enhanced_image,_ = self.DCE_net(data_lowlight)    
         return self.torch_tensor_to_image(enhanced_image)
 
     
